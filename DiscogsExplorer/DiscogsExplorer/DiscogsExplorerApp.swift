d814d3fe0e001584ffa99c9de05a5cf8a739c5c1
Daniel Garcia <danielgarciac.icc@gmail.com>
2025-07-16 13:03:12 -0600

    Initial Commit to Discogs Explorer Challenge

---
+++ DiscogsExplorer/DiscogsExplorer/DiscogsExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscogsExplorerApp.swift
+//  DiscogsExplorer
+//
+//  Created by Daniel Garcia on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DiscogsExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
